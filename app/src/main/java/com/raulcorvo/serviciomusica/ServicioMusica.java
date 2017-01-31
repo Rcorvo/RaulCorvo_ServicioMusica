e2040bdd47c495c61dfc25ac410200d2c8648000
Rcorvo <rcu1987@gmail.com>
2017-01-31 16:04:47 +0100

    Actividad 8.1 terminada

---
+++ app/src/main/java/com/raulcorvo/serviciomusica/ServicioMusica.java
@@ -0,0 +1,43 @@
+package com.raulcorvo.serviciomusica;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by Raul Corvo on 26/01/2017.
+ */
+
+public class ServicioMusica extends Service {
+
+    MediaPlayer reproductor;
+
+    @Override
+    public void onCreate(){
+        Toast.makeText(this, "Servicio creado", Toast.LENGTH_SHORT).show();
+        reproductor = MediaPlayer.create(this, R.raw.audio);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int idArranque){
+        Toast.makeText(this, "Servicio arrancado " + idArranque,
+                Toast.LENGTH_SHORT).show();
+        reproductor.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy(){
+        Toast.makeText(this, "Servicio detenido", Toast.LENGTH_SHORT).show();
+        reproductor.stop();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
