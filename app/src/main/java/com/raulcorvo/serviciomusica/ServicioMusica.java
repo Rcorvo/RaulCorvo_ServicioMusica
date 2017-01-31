fa6df4826d68c056827ce326b0dd44aca2985b78
Rcorvo <rcu1987@gmail.com>
2017-01-31 16:49:47 +0100

    Actividad 8.3 terminada

--- app/src/main/java/com/raulcorvo/serviciomusica/ServicioMusica.java
+++ app/src/main/java/com/raulcorvo/serviciomusica/ServicioMusica.java
@@ -1,5 +1,8 @@
 package com.raulcorvo.serviciomusica;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -13,30 +16,50 @@ import android.widget.Toast;
 
 public class ServicioMusica extends Service {
 
+    private NotificationManager notificationManager;
+    private static final int ID_NOTIFICATION = 1;
+
     MediaPlayer reproductor;
 
     @Override
     public void onCreate(){
         Toast.makeText(this, "Servicio creado", Toast.LENGTH_SHORT).show();
         reproductor = MediaPlayer.create(this, R.raw.audio);
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
 
-    @Override public void onStart(Intent intent, int startId) {
+    /*@Override public void onStart(Intent intent, int startId) {
         Toast.makeText(this, "Servicio arrancado " + startId, Toast.LENGTH_SHORT).show();
         reproductor.start();
-    }
+    }*/
 
-    /*@Override
+    @Override
     public int onStartCommand(Intent intent, int flags, int idArranque){
         Toast.makeText(this, "Servicio arrancado " + idArranque,
                 Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        long vibrate[] = {0, 100, 200, 300};
+        Notification notification = new Notification.Builder(this)
+                        .setContentTitle("Reproduciendo música")
+                        .setContentText("Información adicional")
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setWhen(System.currentTimeMillis())
+                        .setContentIntent(pendingIntent)
+                        .setVibrate(vibrate)
+                        .setLights(0xffff00ff,0,0)
+                        .getNotification();
+        notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+        notificationManager.notify(ID_NOTIFICATION, notification);
         reproductor.start();
         return START_STICKY;
-    }*/
+    }
 
     @Override
     public void onDestroy(){
         Toast.makeText(this, "Servicio detenido", Toast.LENGTH_SHORT).show();
+        notificationManager.cancel(ID_NOTIFICATION);
         reproductor.stop();
     }
 
